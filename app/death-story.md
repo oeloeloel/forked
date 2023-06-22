f1dadedae9bdb34fd0baa8aea5a66e33f56fc1c9
oeloeloel <leo.prendergast@gmail.com>
2023-06-22 11:08:36 -0400

    before changing text array to hash with paragraphs

---
+++ app/death-story.md
@@ -0,0 +1,151 @@
+# Death Story
+
+## Doctor's Office {#doctor}
+
+The doctor's office has a splendid view of the mountain. You've always dreamed about getting to that peak some day. Maybe you will, one day, when you have time.
+
+Silhouetted against the window, the doctor's features are unreadable.
+
+The doctor says "I'm afraid I have some very bad news. You have 30 seconds left to live. 
+
+"By the time I finish talking, you will have 20 seconds left so, sorry about that. 
+
+"By clicking the button below, you accept that the hospital is not responsible."
+
+[Continue](#bucket)
+
+## Bucket List {#bucket}
+
+Tick tock.
+
+[JUMP OUT OF A PLANE](#airport_jump)
+[TELL SWEETHEART "I LOVE YOU"]
+[RIDE A CROCODILE](#lake)
+[ROB A BANK](#bank)
+[SEE THE WORLD](#airport_world)
+[ORDER THE MOST EXPENSIVE THING ON THE MENU](#restaurant)
+[GAMBLE EVERYTHING AND LOSE](#casino)
+[GO TO THE TOP OF A BIG MOUNTAIN](#mountain)
+[TAKE PART IN AN ILLEGAL CAGE FIGHT](#cage)
+
+## International Airport {#airport_jump}
+You are waiting at the International Airport for any plane that can take you straight up in the air. It's so exciting to think about skydiving back to earth. But the wait is very long. And boring.
+
+Time passes.
+
+You sit and look at your phone.
+
+[Check your Bucket List](#bucket)
+
+## International Airport {#airport_world}
+You are waiting at the International Airport for the first plane out. It's so exciting to think about the places you will see. But the wait is very long. And boring.
+
+Time passes.
+
+You sit and look at your phone.
+
+[Check your Bucket List](#bucket)
+
+## The First Bank You Could Find {#bank}
+You are at the first bank you could find.
+
+The teller glances up at you and back down at the huge pile of green paper on their desk.
+
+"Give me all your money!" you shout.
+
+The teller looks up again but this time without lifting their head.
+
+"Why?" they ask.
+
+"Oh, good point", you reply.
+
+[Check your Bucket List](#bucket)
+
+
+
+## Restaurant Très Cher {#restaurant}
+You were lucky to get a table at the swankiest, most avant garde restaurant in town.
+
+A snooty waiter approaches.
+
+The waiter asks, "May I ask how you will be paying today?"
+
+"Oh, good point", you reply.
+
+[Check your Bucket List](#bucket)
+
+
+## Casino {#casino}
+You walk into the casino feeling lucky.
+
+A croupier beckons you over to the roulette table.
+
+"Do you have any money?" they ask. You shake your head.
+
+"If you don't have any money, I recommend that you get some money. And then you bring the money here. And then you gamble all that money on the roulette wheel. And then you do it again until you have no money. It's a LOT of fun."
+
+[Check your Bucket List](#bucket)
+
+## Mount Charlie {#mountain}
+
+In front of you stands the impressive Mount Charlie. This mountain is your destiny. You have wanted to reach its summit since you were old enough to have pointless ambitions.
+
+But you never had the the time. You still don't, but you're going to try it anyway.
+
+You find a hand-hold. Then you find a foot-hold. Then you find a second hand-hold. Then you realise you're holding your own hand.
+
+Oh boy, this is tiring. You find a nice comfortable spot to sit down.
+
+[Check your Bucket List](#bucket)
+
+## Illegal Cage Fighting Venue {#cage}
+
+It's not easy to find an illegal cage fighting venue at short notice, but you managed it.
+
+The referee is speaking.
+
+"In the red corner! The world champion of illegal cage fighting! The Masked Lump!
+
+"Who is brave enough! To enter this illegal cage! And fight The Masked Lump!"
+
+You step into the cage. The referee says "I want a good! Clean! Fight! Now shake hands!".
+
+[Fight a Good Clean Fight](#clean_fight)
+[Fight a Low-Down Dirty Fight](#dirty_fight)
+
+## Illegal Cage Fighting Venue {#clean_fight}
+<inventory_add "trampoline">
+Without stopping to shake hands, The Masked Lump kicks you in the private parts. You buckle over and drop to the ground, clutching your nether region and crying tears of pain.
+
+"Congratulations!" the referee shouts at you. "You won second prize!"
+
+"A trampoline!"
+
+The trampoline fits nicely in your pocket!
+
+[Admire your Bucket List](#bucket)
+
+## Illegal Cage Fighting Venue {#dirty_fight}
+<money_add 1000>
+Without stopping to shake hands, you punch The Masked Lump in the private parts. Your opponent buckles over and drops to the ground, clutching their nether region and crying tears of pain.
+
+"Congratulations!" the referee shouts at you. "You won first prize!"
+
+"One thousand dollars!"
+
+You are now richer by $1,000!
+
+[Admire your Bucket List](#bucket)
+
+## Crocodile Lake {#lake}
+
+There must be a crocodile around here somewhere. Why else would it be called Crocodile Lake?
+
+"Hey crocodiles!" You yell. "I'm right here! Come and get me!".
+
+You didn't really think this plan through.
+
+Fortunately, no crocodiles take you up on your offer.
+
+[Check your Bucket List](#bucket)
+
--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,10 @@
 $gtk.reset
 
+# TODO: description fails if it does not contain newlines
+
 # STORY_FILE = 'app/story.md'
-STORY_FILE = 'app/peas.md'
+# STORY_FILE = 'app/peas.md'
+STORY_FILE = 'app/death-story.md'
 
 def tick args
   defaults args unless args.state.defaults_set
@@ -9,7 +12,10 @@ def tick args
   calc args
   render args
 
-  args.gtk.reset_next_tick if args.inputs.keyboard.key_down.backspace
+  if args.inputs.keyboard.key_down.backspace ||
+     args.state.reset_at_end_of_tick
+    args.gtk.reset_next_tick 
+  end
 end
 
 def defaults args
@@ -19,16 +25,22 @@ def defaults args
   args.state.line_height = 26
 
   args.state.story = fetch_story args
- 
+
   args.state.root_node = args.state.story[:content][0]
   args.state.title = args.state.story[:title]
-  
-  follow args
 
+
+  # death story variables
+  args.state.death_story.money = 0
+  args.state.death_story.inventory = []
+
+
+  follow args
   args.state.defaults_set = true
 end
 
 def input args
+  return unless args.state.options
   return if args.state.options.empty?
 
   args.state.options.each do |o|
@@ -44,6 +56,17 @@ def calc args
 end
 
 def follow args, option = nil
+  if option
+    if option.action
+      if option.action.start_with? "#"
+        # "it's a link"
+      else
+        eval (option.action.to_s + " args")
+        return
+      end
+    end
+  end
+
   args.state.current_node =
     if option
       args.state.story.content.select { |k| k[:id] == option.action }[0]
@@ -54,6 +77,9 @@ def follow args, option = nil
   args.state.current_choices = args.state.current_node[:choices] || []
   args.state.options = []
   args.state.current_heading = args.state.current_node[:heading] || ''
+
+  args.state.current_node.commands.each { |c| eval c }
+  raise "Current description is not set in follow method. Current node is #{args.state.current_node}" if args.state.current_description.nil?
 end
 
 def carve args, paragraphs
@@ -91,26 +117,41 @@ end
 
 def render args
   line_height = args.state.line_height
-  y_position = args.state.display_margin_y.from_top
+  y_position = args.state.display_margin_y.from_top - line_height
+
+  args.state.options = []
+  options = []
 
   labels = []
+  lines  = []
 
   heading = args.state.current_heading
   heading = args.state.title if heading.empty?
   labels << {
     x: 20, y: y_position,
     text: heading,
-    vertical_alignment_enum: 2
+    vertical_alignment_enum: 0
   }
 
+  lines << {
+    x: args.state.display_margin_x,
+    y: y_position,
+    w: args.state.display_w,
+    h: 0
+  }.line!
+
   y_position -= line_height
+
+  if args.state.current_description.nil?
+    raise "No current description available. Cannot map it."
+  end
 
   labels << args.state.current_description.map do |line|
     y_position -= line_height
     {
       x: 20, y: y_position,
       text: line,
-      vertical_alignment_enum: 2
+      vertical_alignment_enum: 0
     }
   end
 
@@ -118,49 +159,101 @@ def render args
 
   unless args.state.current_choices.empty?
     args.state.options = []
+    options = []
 
     args.state.current_choices.each do |o|
       opt_text = " > #{o.choice}"
       box = args.gtk.calcstringbox opt_text
       y_position -= line_height * 1.5
 
-      args.state.options << {
+      if o.destination.nil?
+        opt_color = { r: 0, g: 0, b: 0 }
+        bg_color = { r: 200, b: 200, g: 200 }
+
+        lines << {
+          x: args.state.display_margin_x, 
+          y: y_position + line_height.half, 
+          w: box[0] + 5, h: 0
+        }
+      else
+        opt_color = { r: 255, g: 255, b: 255 }
+        bg_color = { r: 0, g: 0, b: 0}
+      end
+
+      option = {
         x: args.state.display_margin_x,
-        y: y_position - line_height + 2,
+        y: y_position - 2,
         w: box[0] + 5,
         h: line_height,
+        **bg_color,
         action: o.destination
       }
+      
+      options << option
+      args.state.options << option unless o.destination.nil?
 
       labels << {
         x: args.state.display_margin_x,
         y: y_position,
         text: opt_text,
-        vertical_alignment_enum: 2,
-        r: 255, g: 255, b: 255
+        vertical_alignment_enum: 0,
+        **opt_color
       }
+
     end
 
     args.outputs.solids << args.state.options
   end
 
+  restart_text = " < Begin again"
+
+  restart_label = {
+    x: args.state.display_margin_x,
+    y: args.state.display_margin_y,
+    text: restart_text,
+    r: 255, g: 255, b: 255,
+    vertical_alignment_enum: 0
+  }
+
+  box = args.gtk.calcstringbox(restart_text)
+
+  restart_option = {
+    x: restart_label.x,
+    y: restart_label.y - 2,
+    w: calcstringwidth(restart_text) + 5, h: line_height,
+    action: "reset_game"
+  }
+
+  labels << restart_label
+  options << restart_option
+  args.state.options << restart_option
+
+  args.outputs.solids << options
   args.outputs.labels << labels
+  args.outputs.lines << lines
 end
 
 def fetch_story args
   story_text = args.gtk.read_file STORY_FILE
+  
+  if story_text.nil?
+    raise "The file #{STORY_FILE} failed to load. Please check it."
+  end
+
   parse_story args, story_text
 end
 
-def parse_story args, story_file
+def parse_story args, story_text
   story = { content: [] }
   current_heading_number = -1
   temp_description = ''
   flush_description = false
 
-  story_file.each_line.with_index do |l, i|
+  story_text.each_line.with_index do |l, i|
     l.strip!
-    if l.start_with? '##'
+    if l.start_with? '<'
+      story[:content][current_heading_number][:commands] << new_command(args, l)
+    elsif l.start_with? '##'
       # found heading
       current_heading_number += 1
       story[:content] << new_heading(args, l)
@@ -169,14 +262,14 @@ def parse_story args, story_file
       # found title
       story[:title] = l[2..l.length]
     elsif l.start_with? '['
-      # found link
+      # plutz "# found link"
       story[:content][current_heading_number][:choices] << new_choice(l)
     else
       # found description
       temp_description += "\n" + l
     end
 
-    if flush_description || i == story_file.size - 1 # eof
+    if flush_description || i == story_text.size - 1 # eof
       story[:content][current_heading_number - 1][:description] = new_description(args, temp_description)
       temp_description = ''
       flush_description = false
@@ -184,8 +277,14 @@ def parse_story args, story_file
   end
 
   story[:content][current_heading_number][:description] = new_description(args, temp_description)
-
+  
   story
+end
+
+def new_command args, line
+  line.delete!('<>')
+  line.strip!
+  line.split(' ').join(' args, ')
 end
 
 def new_heading args, line
@@ -193,12 +292,18 @@ def new_heading args, line
   heading, id = line.split('{')
   heading.strip!
 
+  if id.nil? && heading
+    raise "The heading \"#{heading}\" must be followed by an id. Example:
+## The Bottom Of the Ocean {#bottom_of_ocean}"
+  end
+
   id.chomp!('}').strip!
   {
     heading: heading,
     id: id,
     choices: [],
-    description: []
+    description: [],
+    commands: []
   }
 end
 
@@ -208,14 +313,52 @@ end
 
 def new_choice line
   choice, destination = line.strip.split(']')
+  if choice.nil?
+    raise "No choice was found. Line is #{line}"
+  end
+  unless destination.nil?
+    destination.delete!('()').strip!
+  end
   choice.delete!('[').strip!
-  destination.delete!('()').strip!
+  
   {
     choice: choice,
     destination: destination
   }
 end
 
+def reset_game args
+  args.state.reset_at_end_of_tick = true
+end
+
 def calcstringwidth str
   $gtk.calcstringbox(str)[0]
+end
+
+#####################
+# CUSTOM FUNCTIONS
+#####################
+
+def inventory_add args, item
+  args.state.death_story.inventory << item
+end
+
+def inventory_remove args, item
+  args.state.death_story.inventory.delete(item)
+end
+
+def inventory_has? args, item
+  args.state.death_story.inventory.include? item
+end
+
+def money_add args, amount
+  args.state.death_story.money += amount
+end
+
+def money_remove args, amount
+  args.state.death_story.money -= amount
+end
+
+def money_amount args, amount
+  args.state.death_story.money
 end
