0085e04c65722af7cb3ad783d9c7a613e724a941
oeloeloel <leo.prendergast@gmail.com>
2024-02-11 11:35:14 -0500

    Cleanup tick.rb, add comments; load story file on tick 1 for web player

--- app/tick.rb
+++ app/tick.rb
@@ -1,39 +1,61 @@
+#########################################################################
+# STORY FILE
+# ----------
+# Edit below to change the story file
+# Adding a `#` at the start of a line will stop a story file from loading
+# Removing the `#` from the start of a line will make the story file load
+# Only one story file will load
+#########################################################################
+
+# This is the user manual
 STORY_FILE = 'app/story.md'
+
+# A story about three peas, written by Raymond Queneau
 # STORY_FILE = 'app/peas.md'
+
+# A fragment of a ghost story
 # STORY_FILE = 'app/threshold.md'
 
+########################################
+# THEMES
+# ------
+# Edit below to change the display theme
+########################################
+
+# Light background, dark text
 # THEME = LIGHT_MODE
-THEME = DARK_MODE
-# THEME = KIFASS_THEME
+
+# Dark background, light text
+# THEME = DARK_MODE
+
+# Vibrant colours inspired by the KIFASS game jam
+THEME = KIFASS_THEME
+
+# Colour scheme inspired by the 20 Second Game Jam
 # THEME = TWENTY_SECOND_THEME
 
+#################
+# TICK METHOD
+# -----------
+# Loads the story
+#################
 def tick args
-  $timer_start = Time.now.to_f
+  # on the html5 player, the story file loads faster if it runs after tick 0
+  # the code below prevents Forked from running on tick 0 if the game is running on the web
+  # this is a good place to add a splash/loading screen if you want one
+  # but, generally, loading will be complete before the player has time to read any text
+  if args.tick_count.zero? && args.gtk.platform?(:web)
+    # display a black background color so we don't get a white flash
+    args.outputs.background_color = { r: 0, g: 0, b: 0 }
+    return 
+  end
 
+  # load the story file specified at the top of this page
   $story ||= Forked::Story.new
-  args.outputs.background_color = [51, 51, 51]
-  
+
+  # keep the story running every tick
   $story.args = args
   $story.tick
-
-  if args.inputs.keyboard.key_held.nine
-    args.outputs.labels << [10, 600, "%0.4f" % [$tick_time]].label
-    args.outputs.labels << {
-      x: 640, y: 360,
-      text: "#{args.gtk.current_framerate_render} fps render, #{args.gtk.current_framerate_calc} fps simulation",
-      size_enum: 20,
-      r: 255, g: 0, b: 0, alignment_enum: 1, vertical_alignment_enum: 1 }
-    args.outputs.primitives << args.gtk.current_framerate_primitives
-  end
-  
-  args.state.forked.forked_show_eval ||= false
-  if args.inputs.keyboard.key_held.two && args.inputs.keyboard.key_down.three
-    args.state.forked.forked_show_eval = !args.state.forked.forked_show_eval
-    puts args.state.forked.forked_show_eval 
-  end
-  $gtk.reset if args.inputs.keyboard.key_down.backspace
-
-  $tick_time = Time.now.to_f - $timer_start 
 end
 
 $gtk.reset
