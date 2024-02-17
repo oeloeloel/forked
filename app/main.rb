0c309b1b4a25a0c76f10185471e0da15e82fc559
oeloeloel <leo.prendergast@gmail.com>
2024-02-17 08:59:24 -0500

    Shuffle files: Forked now uses require relative to load files so that the user's tick method can be more easily managed. In the Forked repo, Forked itself has moved from app/forked to lib/forked

--- app/main.rb
+++ app/main.rb
@@ -1,17 +1,2 @@
-require 'app/libs/json.rb'
-
-require 'app/forked/defaults.rb'
-require 'app/forked/tools.rb'
-require 'app/forked/themes/light-mode-theme.rb'
-require 'app/forked/themes/dark-mode-theme.rb'
-require 'app/forked/themes/kifass-theme.rb'
-require 'app/forked/themes/20-second-theme.rb'
-require 'app/forked/parse.rb'
-require 'app/forked/input.rb'
-require 'app/forked/display_defaults.rb'
-require 'app/forked/display.rb'
-require 'app/forked/author.rb'
-require 'app/forked/forked.rb'
-require 'app/forked/export.rb'
-
+require 'lib/forked/forked.rb'
 require 'app/tick.rb'
