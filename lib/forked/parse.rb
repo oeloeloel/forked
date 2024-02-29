7417cf4bce4e970ea2492fb4d7a8771903da1b7a
oeloeloel <leo.prendergast@gmail.com>
2024-02-28 20:36:46 -0500

    parse blank lines into :blank elements used to end runs of similar elements

--- lib/forked/parse.rb
+++ lib/forked/parse.rb
@@ -129,6 +129,11 @@ module Forked
 
           # PARAGRAPH
           parse_paragraph(line, context, story, line_no)
+
+          # MEANINGFUL BLANK LINE
+          result = parse_blank(line, context, story, line_no)
+          next if result
+          # putz "Line: #{line_no + 1} '#{line}', #{context}"
         end
 
         story
@@ -165,6 +170,24 @@ module Forked
         end
 
         true
+      end
+
+      # if a line is totally blank, add it as a `:blank` element
+      # only add one :blank - runs of blanks are not meaningful
+      # display will ignore the blank
+      # display will not treat two of the same block level
+      # elements as contiguous if they are separated by a blank
+      def parse_blank(line, context, story, line_no)
+        # apply to blank lines and NO CONTEXT IS OPEN
+        return if !line.strip.empty? ||
+                  !context.empty?
+        
+        # check last element type
+        prev_type = story&.[](:chunks)[-1][:content]&.[](-1)[:type]
+
+        # if blank is meaningful after last element, add it
+        # for now, blank is only meaningful after button
+        story[:chunks][-1][:content] << make_blank_hash if prev_type == :button
       end
 
       # Force a line to display as plain, unformatted text, ignoring any further markup
@@ -780,6 +803,12 @@ Please add a title to the top of the Story File. Example:
         return unless idx = line.index(delimiter)
         
         [line[0...idx], line[idx + delimiter.length...line.length]]
+      end
+
+      def make_blank_hash
+        {
+          type: :blank
+        } 
       end
 
       def make_slug(line)
