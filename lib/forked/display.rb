9402e886eb7c78820ae6b919c4366cd1532dfb33
oeloeloel <leo.prendergast@gmail.com>
2024-03-08 13:19:17 -0500

    add image display

--- lib/forked/display.rb
+++ lib/forked/display.rb
@@ -218,6 +218,8 @@ module Forked
         when :button
           next_y_pos = display_button(y_pos, item, content, i)
           highlight_selected_option
+        when :image
+          next_y_pos = display_image(y_pos, item)
         when :blank
         else
         end
@@ -441,6 +443,37 @@ module Forked
       }.sprite!(rule)
 
       y_pos -= rule.spacing_after
+    end
+
+    def display_image(y_pos, item)
+      image = data.style.image
+      display = data.style.display
+
+      # I don't know why path doesn't work *as is*
+      # It's a string, and it doesn't have any odd
+      # characters in it
+      # But putting it into a _new_ string makes it work
+      path = "#{item[:path]}"
+      merger = {}
+      if path.end_with?('}') && path.include?('{')
+        path, remnant = Forked::Parser.pull_out('{', '}', path)
+        merger = eval ('{' + remnant + '}')
+      end
+
+      h = merger.h || 80
+      w = merger.w || 80
+
+      im = {
+        x: display.margin_left,
+        y: y_pos.to_i - h,
+        w: 80,
+        h: h,
+        path: path,
+      }.sprite!(image)
+
+      data.primitives << im.merge(merger)
+
+      y_pos -= (h + image.spacing_after)
     end
 
     def display_code_block(y_pos, item, previous_element_type)
