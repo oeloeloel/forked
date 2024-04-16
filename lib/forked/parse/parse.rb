db41ea3521af73813a3ac66f25904111745379ca
oeloeloel <leoprendergast@gmail.com>
2024-04-16 09:32:23 -0400

    bugfix: code fence was not removed inside chunk action, causing exception

--- lib/forked/parse/parse.rb
+++ lib/forked/parse/parse.rb
@@ -320,7 +320,7 @@ module Forked
       def parse_code_fence(line, context, story, line_no)
         return unless line.include?('```') 
         
-        prohibited_contexts = [:code_block, :action_block]
+        prohibited_contexts = [:code_block]
         mandatory_contexts = []
         return unless context_safe?(context, prohibited_contexts, mandatory_contexts)
 
